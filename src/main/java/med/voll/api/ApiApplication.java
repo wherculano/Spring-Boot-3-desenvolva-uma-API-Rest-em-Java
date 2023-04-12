e56c935bd59cce0cbf10688bf522f6e9d8c54e6d
wagner <wagner.santos@accurate.com.br>
2023-04-12 14:40:39 -0300

    Configurado acesso ao banco de dados e leitura do arquivo .env

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -3,11 +3,32 @@ package med.voll.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        loadEnvVariables();
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+    private static void loadEnvVariables() {
+        // Carrega as variáveis do arquivo ".env"
+        Properties props = new Properties();
+        try (InputStream input = new FileInputStream(".env")) {
+            props.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        props.forEach((key, value) -> {
+            System.setProperty((String) key, (String) value);
+        });
+    }
 
 }
