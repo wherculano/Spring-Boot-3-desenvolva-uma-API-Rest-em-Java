d51b973cb6452a17aa933a914d1de03f508dd814
Wagner Herculano <wagherculano@hotmail.com>
2023-04-13 22:17:51 -0300

    Configurado endpoint para atualizacao cadastral

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -51,4 +51,16 @@ public class Medico { //@EqualsAndHashCode(of = "id") foi removido pela ID e imp
     public int hashCode() {
         return getClass().hashCode();
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 }
